3a9b57f780dad8f904268d3647c9a741e19a2f65
Ahmed Ismail <144972161+ahmedismaiill@users.noreply.github.com>
2026-02-16 05:26:16 +0200

    Integrate LLM for local model setup
    Updated model setup to use LLM from crewai and configured for Streamlit deployment.

--- Modeling/model_setup.py
+++ Modeling/model_setup.py
@@ -1,10 +1,17 @@
 from configuration import *
-
+from crewai import LLM
 # ============= MODEL SETUP ============= #
 embedding_model_name = "sentence-transformers/all-MiniLM-L6-v2"
 
 # --- LLM (Ollama local model) --- #
-llm = Ollama(model="llama3.2:3b")
+# llm = Ollama(model="llama3.2:3b")
+
+# ========== for streamlit deployment ========== #
+language_model = LLM(
+    model="ollama/llama3.2:3b",
+    base_url="http://localhost:11434",
+    temperature=0.9,
+)
 
 # ========== EMBEDDINGS ========== #
 embedding_model = HuggingFaceEmbeddings(
